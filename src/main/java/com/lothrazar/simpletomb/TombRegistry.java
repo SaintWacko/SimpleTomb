5704d16e2943deec0e5715de3bf881b36783698d
Mrbysco <Mrbysco@users.noreply.github.com>
2021-12-02 14:46:29 +0100

    Remove unused EventBusSubscriber

--- src/main/java/com/lothrazar/simpletomb/TombRegistry.java
+++ src/main/java/com/lothrazar/simpletomb/TombRegistry.java
@@ -10,12 +10,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.material.Material;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TombRegistry {
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModTomb.MODID);
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModTomb.MODID);
